d1bbe0d6dcf65da9ecbb9a5779fcf33c8b61ee6c
xeony <71081051+xeony888@users.noreply.github.com>
2025-04-07 00:30:40 -0400

    stuff

--- reload.sh
+++ reload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source $HOME/.cargo/bin
-git pull origin main && cargo build --release && sudo systemctl restart warp-server && echo "Deployment complete"
+git pull && cargo build --release && sudo systemctl restart warp-server && echo "Deployment complete"
